62beab6402332c3473c0f7fc2978219356de49c1
gpteamcs <ahmedfcisasu@gmail.com>
2018-09-11 20:12:08 +0200

    Adding delete methods

--- playground/mongodb-delete.js
+++ playground/mongodb-delete.js
@@ -1,32 +1,22 @@
 //const MongoClient = require('mongodb').MongoClient;
-const {MongoClient, ObjectID} = require('mongodb');
+const { MongoClient, ObjectID } = require('mongodb');
 
 
 MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, client) => {
     if (err)
         return console.log('Unable to connect to MongoDB Server');
     console.log('Connected to MongoDB Server');
-   // const db = client.db("TodoApp");
-    // db.collection('Todos').insertOne({
-    //     "text": "Test",
-    //     "completed": false
+    const db = client.db("TodoApp");
+    // deleteMany
+    // db.collection('Todos').deleteMany({ text: 'Eat Lunch' }).then((result) => { console.log(result) });
+    // deleteOne
+    //  db.collection('Todos').deleteOne({ text: 'Eat Lunch' }).then((result) => { console.log(result) });
 
-    // }, (err, result) => {
-    //     if (err)
-    //         return console.log('Unable to insert todo', err);
-    //     console.log(JSON.stringify(result.ops, undefined, 2));
-    // });
 
-    // db.collection('Users').insertOne({
-    //     "name": "som3a",
-    //     "age": 12,
-    //     "location": "Giza"
-    // }, (err, result) => {
-    //     if (err)
-    //         return console.log("Unable to insert user", err);
-    //     console.log(JSON.stringify(result.ops, undefined, 2));
+    //findOneAndDelete
 
-    // });
+    db.collection('Todos').findOneAndDelete({ completed: false }).then((result) => {
+        console.log(result);
+    });
     client.close();
-    //    db.close();
 });
