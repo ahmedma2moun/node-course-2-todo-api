34188dcad4584a463a9c654c3832e3f2774054b1
gpteamcs <ahmedfcisasu@gmail.com>
2018-09-11 21:05:58 +0200

    Adding update methods

--- playground/mongodb-update.js
+++ playground/mongodb-update.js
@@ -7,9 +7,22 @@ MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, client) => {
         return console.log('Unable to connect to MongoDB Server');
     console.log('Connected to MongoDB Server');
     const db = client.db("TodoApp");
-    
+
     // findOnAndUpdate
-db.collection('Todos').findOneAndUpdate()
+    db.collection('Todos').findOneAndUpdate(
+        {
+            _id: new ObjectID("5b1ba17bd59d5998e88e8dff")
+        },
+        {
+            $set: {
+                completed: false
+            }
+        },
+        {
+            returnOriginal: false
+        }).then((result) => {
+            console.log(result);
+        });
 
 
     client.close();
