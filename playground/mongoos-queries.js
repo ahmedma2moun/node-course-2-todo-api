0a71a6592929dba6b54bfb85d0810a7bef0cac4e
gpteamcs <ahmedfcisasu@gmail.com>
2018-09-15 17:37:17 +0200

    add files

---
+++ playground/mongoos-queries.js
@@ -0,0 +1,25 @@
+const { mongoose } = require('./../server/db/mongoose');
+const { Todo } = require('./../server/models/todo');
+const { ObjectID } = require('mongodb')
+
+var id = "5b9d2316b948b5901fa4aacz";
+if(!ObjectID.isValid(id))
+{
+    console.log('ID not valid');
+    return;
+}
+// Todo.find({
+//     _id: id
+// }).then((todos) => {
+//     console.log('Todos', todos);
+// });
+
+// Todo.findOne({
+//     _id: id
+// }).then((todo) => {
+//     console.log('Todo', todo);
+// });
+
+Todo.findById(id).then((todo) => {
+    console.log('Todo by id', todo);
+}).catch((e) => console.log(e));
