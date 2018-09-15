38c1dddea1ccf739a2b727453d5841cd84d6a0c9
gpteamcs <ahmedfcisasu@gmail.com>
2018-09-15 18:06:48 +0200

    add Get todos\:id test cases

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -1,12 +1,13 @@
 const expect = require('expect');
 const request = require('supertest');
+const {ObjectID} = require('mongodb');
 
 const { app } = require('./../server');
 const { Todo } = require('./../models/todo');
 
 const todos = [
-    { text: 'First Test todo' },
-    { text: 'Second Test todo' }
+    { text: 'First Test todo' , _id : new ObjectID() },
+    { text: 'Second Test todo' , _id : new ObjectID()}
 ];
 
 beforeEach((done) => {
@@ -66,5 +67,38 @@ describe('Get /todos', () => {
             expect(res.body.todos.length).toBe(2);
         })
         .end(done);
-    })
+    });
+
+    
+});
+
+
+describe('Get /todos/:id', () => {
+    it('Shoud returnd tood doc',(done)=>{
+        request(app)
+        .get(`/todos/${todos[0]._id.toHexString()}`)
+        .expect(200)
+        .expect((res)=>{
+            expect(res.body.todo.text).toBe(todos[0].text);
+        })
+        .end(done);
+    });
+
+    it('Shoud returnd 404 not fine doc',(done)=>{
+        var hexId = new ObjectID().toHexString()
+        request(app)
+        .get(`/todos/${hexId}`)
+        .expect(404)
+        .end(done);
+    });
+
+    it('Shoud returnd 404 non Object id',(done)=>{
+        var hexId = new ObjectID().toHexString()
+        request(app)
+        .get(`/todos/123abc`)
+        .expect(404)
+        .end(done);
+    });
+
+    
 });
