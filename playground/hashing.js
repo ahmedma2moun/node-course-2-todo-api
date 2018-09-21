ed131fc4f269934cfb92153b5c3083f92486c2a4
gpteamcs <ahmedfcisasu@gmail.com>
2018-09-21 23:56:07 +0200

    hashing passswords

--- playground/hashing.js
+++ playground/hashing.js
@@ -1,13 +1,28 @@
 const { SHA256 } = require('crypto-js');
-const jwt =require('jsonwebtoken');
-var data = {
-    id: 4
-};
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
 
-var token =jwt.sign(data,'123abc');
-console.log(token);
-var decoded =jwt.verify(token,'123abc');
-console.log('decoded',decoded);
+var password = '123abc';
+// bcrypt.genSalt(10, (err, salt) => {
+//     bcrypt.hash(password, salt, (err, hash) => {
+//         console.log(hash);
+//     });
+// })
+
+var hashedPassword = '$2a$10$bVMpK50AZ9ajFIPBQI6eIuIL/PzpiOPo2hOr7mmNe/edYG1nqg44i';
+
+bcrypt.compare(password, hashedPassword, (err, res) => {
+    console.log(res);
+})
+
+// var data = {
+//     id: 4
+// };
+
+// var token =jwt.sign(data,'123abc');
+// console.log(token);
+// var decoded =jwt.verify(token,'123abc');
+// console.log('decoded',decoded);
 // var message = 'I am user number 3';
 // var hash = SHA256(message).toString();
 
