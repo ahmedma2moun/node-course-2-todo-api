f7afe8ef1af2547fac293c818641578d38c7df29
gpteamcs <ahmedfcisasu@gmail.com>
2018-09-21 23:30:54 +0200

    Add GET user me and auth meddleware

--- server/server.js
+++ server/server.js
@@ -9,7 +9,7 @@ var { ObjectID } = require('mongodb')
 var { mongoose } = require('./db/mongoose');
 var { Todo } = require('./models/todo');
 var { User } = require('./models/user');
-
+var {authenticate} =require('./middleware/authenticate');
 var app = express();
 const port = process.env.PORT;
 app.use(bodyParser.json());
@@ -87,12 +87,18 @@ app.post('/users', (req, res) => {
     user.save().then(() => {
         return user.generateAuthToken();
     }).then((token) => {
-        res.header('x-auth',token).send(user.toJSON());
+        res.header('x-auth', token).send(user.toJSON());
     }).catch((e) => {
         res.status(400).send(e);
     })
 });
 
+
+
+
+app.get('/users/me',authenticate, (req, res) => {
+    res.send(req.user);
+})
 
 app.listen(port, () => {
     console.log(`Started on port ${port}`);
